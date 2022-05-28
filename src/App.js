647fe502cf06d2b8cbc84a736554d55df78dc3b3
fceja <88167774+fceja@users.noreply.github.com>
2022-05-28 14:35:29 -0700

    update main contianer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="search-container">
+        <input type="text" id='search-input' className="search-input" placeholder='Symbol'/>
+      </div>
+      <div className="results-container">
+        <span id="results">Results</span>
+      </div>
     </div>
   );
 }
